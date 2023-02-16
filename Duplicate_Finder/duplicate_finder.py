b4a1fee292f7d8bc3505e3fe60a9e708469b76fd
Viktoria Haghani <62615778+vhaghani26@users.noreply.github.com>
2023-02-16 11:12:56 -0800

    add timer and fix verbosity

--- Duplicate_Finder/duplicate_finder.py
+++ Duplicate_Finder/duplicate_finder.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 import argparse
+import time
+import sys
 import os
 import stat
 
@@ -8,7 +12,7 @@ import stat
 
 # Initialize argparse
 parser = argparse.ArgumentParser(
-	description='Find duplicated files within the specified directory')
+    description='Find duplicated files within the specified directory')
 
 # Required arguments
 parser.add_argument('--path', required=True, type=str,
@@ -28,6 +32,9 @@ parser.add_argument('--verbose', action="store_true",
 # Finalization of argparse
 arg = parser.parse_args()
 
+# Initiate timer
+t0 = time.time()
+
 #########################
 ## Retrieve File Sizes ##
 #########################
@@ -38,50 +45,53 @@ if arg.verbose:
 # Index all files by their size
 size = {}
 for path, subdirs, files in os.walk(arg.path):
-	for name in files:
-		filepath = os.path.join(path, name)
-		mode = os.lstat(filepath).st_mode
-		if not stat.S_ISREG(mode): continue
-		s = os.path.getsize(filepath)
-		if s < arg.min: continue
-		if s not in size: size[s] = []
-		size[s].append(filepath)
+    for name in files:
+        filepath = os.path.join(path, name)
+        mode = os.lstat(filepath).st_mode
+        if not stat.S_ISREG(mode): continue
+        s = os.path.getsize(filepath)
+        if s < arg.min: continue
+        if s not in size: size[s] = []
+        size[s].append(filepath)
 
 #####################
 ## Find Duplicates ##
 #####################
-    
+
+if arg.verbose:
+    print("Calculating psuedo-checksum for potentially duplicated files...", file = sys.stderr)
+        
 # Find duplicate files (1) by file size (2) by pseudo-checksum
-for s in sorted(size, reverse=True):
-    if arg.verbose:
-        print("Finding duplicate files by filtering for file size...", file = sys.stderr)
+for s in sorted(size, reverse=True):    
+    if len(size[s]) == 1: continue
     
-	if len(size[s]) == 1: continue
+    # Create a pseudo-checksum by looking at the head and tail of a file
+    pseudosum = {}
+    for filepath in size[s]:
+        with open(filepath, mode='rb') as fp:
+            head = fp.read(arg.bytes)
+            fp.seek(-arg.bytes, 2)
+            tail = fp.read(arg.bytes)
+            sig = (head, tail)
+            if sig not in pseudosum: pseudosum[sig] = []
+            pseudosum[sig].append(filepath)
     
-    if arg.verbose:
-        print("Calculating psuedo-checksum for potentially duplicated files...", file = sys.stderr)
-    
-	# Create a pseudo-checksum by looking at the head and tail of a file
-	pseudosum = {}
-	for filepath in size[s]:
-		with open(filepath, mode='rb') as fp:
-			head = fp.read(arg.bytes)
-			fp.seek(-arg.bytes, 2)
-			tail = fp.read(arg.bytes)
-			sig = (head, tail)
-			if sig not in pseudosum: pseudosum[sig] = []
-			pseudosum[sig].append(filepath)
-    
-    if arg.verbose:
-        print("Determining duplicates..", file = sys.stderr)
-    
-	# Report duplicates
-	for sig in pseudosum:
-		if len(pseudosum[sig]) == 1: continue
-		ps = None
-		if   s > 1e12: ps = f'{s/1e12:.2f}T'
-		elif s > 1e9:  ps = f'{s/1e9:.2f}G'
-		elif s > 1e6:  ps = f'{s/1e6:.2f}M'
-		elif s > 1e3:  ps = f'{s/1e3:.2f}K'
-		else:          ps = s
-		print(ps, ' '.join(pseudosum[sig]))
+    # Report duplicates
+    for sig in pseudosum:
+        if len(pseudosum[sig]) == 1: continue
+        ps = None
+        if   s > 1e12: ps = f'{s/1e12:.2f}T'
+        elif s > 1e9:  ps = f'{s/1e9:.2f}G'
+        elif s > 1e6:  ps = f'{s/1e6:.2f}M'
+        elif s > 1e3:  ps = f'{s/1e3:.2f}K'
+        else:          ps = s
+        #print(ps, ' '.join(pseudosum[sig]))
+        print(ps)
+        for x in pseudosum[sig]:
+            print("\t", x)
+
+# End timer
+t1 = time.time()
+
+if arg.verbose:
+    print(f"This run took {t1-t0} seconds.", file = sys.stderr)
